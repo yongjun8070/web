17ccae98826961f0b3ccc20bb52f58a09074f6a9
yongjun8070 <107088026+yongjun8070@users.noreply.github.com>
2024-06-11 14:08:44 +0900

    Add files via upload

---
+++ E 레이아웃/script.js
@@ -0,0 +1,25 @@
+$(function(){
+    $(".menu>ul>li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown();
+    })
+    $(".menu>ul>li").mouseout(function(){
+        $(this).find(".submenu").stop().slideUp();
+    })
+    
+})
+setInterval(function(){
+    $(".slidewrap").delay(2000);
+    $(".slidewrap").animate({marginLeft:-795})
+    $(".slidewrap").delay(2000);
+    $(".slidewrap").animate({marginLeft:-1590})
+    $(".slidewrap").delay(2000);
+    $(".slidewrap").animate({marginLeft:0})
+})
+$(function(){
+    $(".notice>ul>li:nth-child(1)").click(function(){
+        $(".popup").show();
+    })
+    $(".button").click(function(){
+        $(".popup").hide();
+    })
+})
